ed07aff6e13e4c780dc428975cd9b6c715fd5084
mehak bera <beramehak0@gmail.com>
2026-08-22 12:57:55 +0530

    Add backend and student problem solving features

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import './index.css';
+import StudentDashboard from "./pages/student/StudentDashboard";
+import ExploreProblems from "./pages/student/ExploreProblems";
+import ChallengeDetails from "./pages/student/ChallengeDetails";
+import SubmitSolution from "./pages/student/SubmitSolution";
+import MySolutions from "./pages/student/MySolutions";    
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/student" element={<StudentDashboard />} />
+        <Route path="/student/problems" element={<ExploreProblems />}/>
+        <Route path="/student/problems/:id" element={<ChallengeDetails />}/>
+        <Route path="/student/problems/:id/submit" element={<SubmitSolution />} />
+        <Route path="/student/solutions" element={<MySolutions />}/>
+
       </Routes>
     </BrowserRouter>
   );
