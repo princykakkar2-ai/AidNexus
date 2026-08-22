17a877d9eef2e15d7b0fe475c5ce389080dec21f
mehak bera <beramehak0@gmail.com>
2026-08-22 17:03:10 +0530

    Integrate backend with MongoDB and API routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,44 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+const problemRoutes = require("./routes/problemRoutes");
+const solutionRoutes = require("./routes/solutionRoutes");
+
+dotenv.config();
+console.log("MONGO_URI loaded:", !!process.env.MONGO_URI);
+
 
 const app = express();
 
-const PORT = 5000;
+// Connect MongoDB
+connectDB();
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
 app.get("/", (req, res) => {
     res.send("AidNexus Backend is running!");
 });
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/problems", problemRoutes);
+app.use("/api/solutions", solutionRoutes);
+
+// 404 handler
+app.use((req, res) => {
+    res.status(404).json({
+        message: "Route not found"
+    });
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
